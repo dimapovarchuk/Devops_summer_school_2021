3729c4971858b18b9884b0cab8969c472214cf02
user <povarchukdima007@gmail.com>
2021-07-17 16:28:41 +0300

    New changes!

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+echo "Start..."
+
+#==============================================================================================
+
+echo "Git add..."
+git add .
+echo "Git added!"
+echo "Git commit..."
+git commit -m "New changes!"
+echo "Git commited!"
+echo "Git push..."
+git push
+echo "Git pushed!"
+
+#==============================================================================================
+
+echo "Finish!"
